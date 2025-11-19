d8cf72f446bd2f4f392a3a67c982c5cb578cd75a
Yaswanth Rahul <yashwanthrahul5126@gmail.com>
2025-11-19 21:28:59 +0100

    types: allow image module imports (jpg/png/svg)

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,8 @@
 /// <reference types="vite/client" />
+
+// Allow importing image assets in TypeScript files
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.svg';
+declare module '*.gif';
